fb63d6f3d19748c689da76a355b4f76319b87a2b
Phone Min Khant <phonekha@z2t13c2.42bangkok.com>
2026-02-04 16:18:11 +0700

    updating header file

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wintoo <wintoo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: phonekha <phonekha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 12:57:01 by wintoo            #+#    #+#             */
-/*   Updated: 2026/02/04 15:20:46 by wintoo           ###   ########.fr       */
+/*   Updated: 2026/02/03 21:35:30 by phonekha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,60 +24,58 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-extern volatile sig_atomic_t	g_signal;
-
 typedef struct s_cmd
 {
-    char			**args;
-    char			*infile;
-	char			*outfile;
-	int				append;
-    struct s_cmd	*next;
-}	t_cmd;
+	char			**args; //{"ls", "-l", NULL}
+	char			*path;  // /bin/ls
+	int				infile; // Default 0 (stdin), or FD from <
+	int				outfile; // Default 1 (stdout), or FD from >
+	bool			piped; // Is there a | after this command ?
+	struct s_cmd	*next; // link to the next command in the pipe
+} t_cmd;
 
-typedef enum e_tktype
+typedef struct	s_env
 {
-	T_WORD,
-	T_PIPE,
-	T_IN,
-	T_OUT,
-	T_APPEND,
-	T_HEREDOC
-}	t_tktype;
-
-typedef struct s_token
-{
+	char			*key;
 	char			*value;
-	t_tktype		type;
-	struct s_token	*next;
-}	t_token;
+	struct s_env	*next;
+}	t_env;
+
+extern volatile sig_atomic_t	g_signal;
 
 //Utils
 void	free2p(char **s);
 void	free1p(char **s);
-void	free_cmds(t_cmd *cmd);
-void	free_tokens(t_token *tok);
 
 //Main
 void	setup_signals(void);
+char	**tokenize(char *line);
 
-//Lexer
-int		skip_spaces(char *s, int *i);
-int		is_operator(char c);
-char	*get_word(char *s, int *i);
-t_token	*new_token(char *val, t_tktype type);
-t_token	*get_token(char *s, int *i);
-t_token	*tokenize(char *line);
+//Built in
+int		is_builtin(char *cmd);
+int		exec_builtin(char **args, t_env **env_copy);
+t_env	*init_env(char **envp);
+int		mini_cd(char **args);
+int		mini_echo(char **args);
+int		mini_pwd(void);
+int		mini_env(t_env *env);
+void	mini_exit(char **args);
+t_env	*find_env_node(t_env *env, char *key);
+void	add_or_update_env(t_env **env, char *key, char *value);
+int		mini_export(char **args, t_env **env);
+int		mini_unset(char **args, t_env **env);
+int		is_valid_var_name(char *str);
+void	sort_env_list(t_env *head);
+void	print_sorted_env(t_env *env);
+t_env	*copy_env_list(t_env *env);
+void	free_env_list(t_env *env);
 
-//Parser
-t_cmd	*new_cmd(void);
-int		count_args(t_token *tok);
-void	fill_args(t_cmd *cmd, t_token *tok);
-void	handle_redir(t_cmd *cmd, t_token *tok);
-t_cmd	*parse_one_cmd(t_token *tok);
-t_token	*skip_to_pipe(t_token *tok);
-t_cmd	*parse(t_token *tok);
 
-int		exe_cmd(char **args, char **envp);
+
+//Execucator
+char	**env_to_array(t_env *env);
+char	*find_path(char *cmd, t_env *env_list);
+int 	exe_cmd(char **args, t_env *env_list);
+
 
 #endif
